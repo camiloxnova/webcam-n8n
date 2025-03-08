011ecab16dc1c9590e26f02b43b009467940baf9
camiloxnova <camilo.xnova360@gmail.com>
2025-03-07 19:02:51 -0500

    Update AvatarResult.tsx

--- src/components/AvatarAi/AvatarResult.tsx
+++ src/components/AvatarAi/AvatarResult.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./AvatarPhoto.scss";
 import avatarImage from "../../assets/img/avatar-2.png";
 import fondo from "../../assets/img/fondo.png";
 import logo from "../../assets/img/claro.png";
 import logor from "../../assets/img/claro-r.png";
 
-const AvatarResult: React.FC = () => {
+interface AvatarResultProps {
+  imageUrl?: string;
+}
+
+const AvatarResult: React.FC<AvatarResultProps> = ({ imageUrl }) => {
+  const [currentImage, setCurrentImage] = useState<string>(avatarImage);
+
+  useEffect(() => {
+    if (imageUrl) {
+      setCurrentImage(imageUrl);
+    }
+  }, [imageUrl]);
+
   return (
     <div className="container">
       <img src={fondo} alt="Fondo" className="fondo" />
@@ -13,7 +25,7 @@ const AvatarResult: React.FC = () => {
         <img src={logo} alt="Logo" className="clarologo" />
         <h2 className="subtitle">AVATAR AI</h2>
         <div className="avatar-container">
-          <img src={avatarImage} alt="Avatar" className="avatar" />
+          <img src={currentImage} alt="Avatar" className="avatar" />
         </div>
         <div className="result">
           <img src={logor} alt="Logo Resultado" className="clarologo" />
