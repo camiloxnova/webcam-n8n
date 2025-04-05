15f6917f500170a0aa61f5182664e43c087402b9
Marco Salazar <marco@honeymustard.mx>
2025-04-05 12:31:59 -0500

    :rocket: [Feat] estilos politica

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
       interval = setInterval(async () => {
         try {
           const response = await fetch(
-            "https://proyectoshm.com/marco_pruebas/imagen/callback.php"
+            "https://proyectoshm.com/marco_pruebas/imagen/callback_dev.php"
           );
           const data = await response.json();
           // Si existe una imagen nueva, se actualiza el estado y se guarda en Firestore.
